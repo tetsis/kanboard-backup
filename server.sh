dd46df5321673c470342e3d9eb1c575284aa22c4
tetsis <hitoshi.zin.nakao@gmail.com>
2017-08-04 10:06:13 +0200

    [add] archive

--- server.sh
+++ server.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+# Backup
 DATE=`LANG=en date "+%Y%m%d"`
 FILENAME="kanboard-data-${DATE}.tar.gz"
 
 mkdir /root/kanboard-backup
 cd /root/kanboard-backup
 
-cp -a /var/www/html .
+cp -a /var/www/html/kanboard .
 
-tar zcvf kanboard-data-${DATE}.tar.gz html
+tar zcvf kanboard-data-${DATE}.tar.gz kanboard
 
-rm -rf html
+rm -rf kanboard
+
+# Archive
+YEAR=`LANG=en date "+%Y"`
+MONTH=`LANG=en date "+%m"`
+DAY=`LANG=en date "+%d"`
+
+cd /var/www/html
+mkdir -p archive/${YEAR}/${MONTH}/${DAY}
+cp -a kanboard/* archive/${YEAR}/${MONTH}/${DAY}/
+
+# できれば読み取り専用にしたい
