fa4e64b127d811116b4e7b5bf1f2d4435e6ee929
tetsis <hitoshi.zin.nakao@gmail.com>
2017-08-04 04:29:44 +0200

    [mod] hostname and password input

--- client.sh
+++ client.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-CMDNAME=`basename $0`
-if [ $# -ne 2 ]; then
-    echo "Usage: $CMDNAME hostname password" 1>&2
-    exit 1
-fi
-
 HOSTNAME=$1
 PASSWORD=$2
 DATE=`LANG=en date "+%Y%m%d"`
--- cron_client.sh
+++ cron_client.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+CMDNAME=`basename $0`
+if [ $# -ne 2 ]; then
+    echo "Usage: $CMDNAME hostname password" 1>&2
+    exit 1
+fi
+
+HOSTNAME=$1
+PASSWORD=$2
+
 PWD=`pwd`
 
-echo "  0  8  *  *  * root ${PWD}/client.sh" >> /etc/crontab
+echo "  0  8  *  *  * root ${PWD}/client.sh ${HOSTNAME} ${PASSWORD}" >> /etc/crontab
